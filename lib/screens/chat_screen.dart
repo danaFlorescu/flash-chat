9aa8886df1717ec77d9f3d992241950bb79cdb85
danaFlorescu <mariadana977@gmail.com>
2023-02-19 12:45:54 +0200

    adding the messages stream

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -35,6 +35,26 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+  // void getMessages() async {
+  //   final messages = await _firestore.collection('messages').get();
+  //
+  //   for (var message in messages.docs) {
+  //     print(
+  //       message.data(),
+  //     );
+  //   }
+  // }
+
+  void messagesStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.docs) {
+        print(
+          message.data(),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,8 +64,9 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                _auth.signOut();
-                Navigator.pop(context);
+                // _auth.signOut();
+                // Navigator.pop(context);
+                messagesStream();
               }),
         ],
         title: Text('⚡️Chat'),
